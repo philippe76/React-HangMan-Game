e4423779f755551db3f4ff30f2e3d5dfb0483704
Philippe Lanougadere <soufiane92230@gmail.com>
2020-10-31 17:20:52 +0100

    display already tried letters

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import words from "../words";
+import Tries from './Tries'
 
 
 const Game = () => {
@@ -7,47 +8,46 @@ const Game = () => {
   const [good_letters, setGood_letters] = useState([]);
   const [bad_letters, setBad_letters] = useState([]);
 
-  // update hangman background url
+
+  // create ref for hangman background url
   const picRef = useRef('0')
   const hangman = {
     background: `url(${require(`../img/hangman${picRef.current}.png`)}) center`
   }
 
+  // HANDLECHANGE METHOD //
   const handleChange = (e) => {
     
     const word_to_find = [...words[0]];
-    const value = e.target.value.toUpperCase();
+    const current_letter = e.target.value.toUpperCase();    
 
-    
+
 
     // fill good or bad letters arrays
-    word_to_find.includes(value) ? 
-    setGood_letters((previous) => {
-      return [...previous, value];
-    }) :
-    setBad_letters((previous) => {
-      return [...previous, value];
-    })
-
-    if (!word_to_find.includes(value)) {
-      picRef.current = bad_letters.length+1;
+    if(word_to_find.includes(current_letter) && !good_letters.includes(current_letter)) {
+      setGood_letters((previous) => {
+        return [...previous, current_letter];   
+      })
     }
-    
+    if(!word_to_find.includes(current_letter) && !bad_letters.includes(current_letter)) {
+      setBad_letters((previous) => {
+        return [...previous, current_letter];   
+      })
+    }    
 
+    // update hangman background url
+    picRef.current = !word_to_find.includes(current_letter) ? bad_letters.length+1 : picRef.current 
+    
     // set red or green input outline depending on letter
-    document.querySelector("input").style.boxShadow = word_to_find.includes(value) ? "0px 0px 2px 2px green" : "0px 0px 2px 2px red";
+    document.querySelector("input").style.boxShadow = word_to_find.includes(current_letter) ? "0px 0px 2px 2px green" : "0px 0px 2px 2px red";
 
   };
-
-
 
   // reset input field and color 400ms after keyup
   const resetInput = () => {
     setTimeout(() => (document.querySelector("input").value = ""), 400);
     setTimeout(() => (document.querySelector("input").style.boxShadow = ""), 400);
   };
-
-
 
 
   return (
@@ -79,13 +79,10 @@ const Game = () => {
               onKeyUp={resetInput}
             />         
             <div className="UsedLetters">
-              {/* <Tries alreadyTried={this.state.alreadyTried} /> */}
+              <Tries alreadyTried={bad_letters} />
             </div>  
         </div>
-        <div className="hangZone" style={hangman} >
-          
-          {/* <div style={hangman} id="hangman" className="col-5"></div> */}
-        </div>
+        <div className="hangZone" style={hangman}></div>
       </section>
     </>
   );
