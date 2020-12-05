55cf7f3b6184c1626c9a34cc3f6c6aec9b8e00bf
Philippe Lanougadere <soufiane92230@gmail.com>
2020-12-05 18:15:46 +0100

    Updates

---
+++ precache-manifest.bcd7d4f36f6e6b3d8abc8e11287be74f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be5cc217e41f4ae1788b00fe842257f",
+    "url": "/React-HangMan-Game/index.html"
+  },
+  {
+    "revision": "6a4ee6ababaa8c55d60e",
+    "url": "/React-HangMan-Game/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "f24fe86ad2cddcb08e95",
+    "url": "/React-HangMan-Game/static/css/main.f54ed474.chunk.css"
+  },
+  {
+    "revision": "6a4ee6ababaa8c55d60e",
+    "url": "/React-HangMan-Game/static/js/2.85144e7d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-HangMan-Game/static/js/2.85144e7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24fe86ad2cddcb08e95",
+    "url": "/React-HangMan-Game/static/js/main.e447c25e.chunk.js"
+  },
+  {
+    "revision": "efb2a85d45201b3bcc62",
+    "url": "/React-HangMan-Game/static/js/runtime-main.fdc76baa.js"
+  },
+  {
+    "revision": "764f6516cb85f673c5ac017bc9852240",
+    "url": "/React-HangMan-Game/static/media/hangman5.764f6516.png"
+  },
+  {
+    "revision": "60164175f6bf702bf7679a4abd3d5455",
+    "url": "/React-HangMan-Game/static/media/hangman6.60164175.png"
+  },
+  {
+    "revision": "c11d2af77f2705d0dfbb00bd14cfa420",
+    "url": "/React-HangMan-Game/static/media/hangman7.c11d2af7.png"
+  },
+  {
+    "revision": "39b9b6b109857c0cae64a424d2f368a6",
+    "url": "/React-HangMan-Game/static/media/hangman8.39b9b6b1.png"
+  }
+]);
